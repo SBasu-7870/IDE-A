4451c92c3f6b8fb73cc2678275e4ed1299bfc3ab
SBasu-7870 <rick05basu@gmail.com>
2023-02-16 20:11:56 +0530

    added room function

--- src/components/IDE.jsx
+++ src/components/IDE.jsx
@@ -1,42 +1,61 @@
-import {useRef} from 'react'
+import {useRef, useEffect, useState} from 'react'
+import { useLocation } from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import * as Y from "yjs";
+import * as monaco from 'monaco-editor';
 import { WebrtcProvider } from 'y-webrtc';
 import { MonacoBinding } from 'y-monaco';
-
+import Navbar from './Navbar';
 
 
 function IDE() {
+  const location = useLocation();
+  const roomId = location.state.roomId;
+
   const editorRef = useRef(null);
+  const initializedRoomsRef = useRef(new Set());
   //Editor->value = YJS Text Value(A text value shared by multiple people)
   // One person deletes text -> Deletes from overall shared text value
   // Handled by YJS (a high performance CRDT(Conflict-Free Replicated Data Type))
 
 
-  function handleEditorDidMount(editor,monaco) { //editor instance and monaco from the <Editor/> (defined by Monaco Editor) 
-     editorRef.current = editor;
+  const handleEditorDidMount = (editor,monaco) => { //editor instance and monaco from the <Editor/> (defined by Monaco Editor) 
+    editorRef.current = editor;
      //Initialise Yjs
+     if (initializedRoomsRef.current.has(roomId)) {
+      return;
+    }
 
      const doc = new Y.Doc(); //collection of shared objects -> Text
      //Connect to peers (or start connection) with WebRTC
-     const provider = new WebrtcProvider("test-room",doc); //room1, room2
-     const type = doc.getText("monaco"); // doc {"monaco" : "what our IDE is showing"}
-
-    
-     const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness); //editorRef.current.getModel()-> monaco specific; allows to see changes happening in Monaco
+     const provider = new WebrtcProvider(roomId,doc); //room1, room2
      console.log(provider.awareness);
+     const type = doc.getText("monaco"); // doc {"monaco" : "what our IDE is showing"}
+      
+      
+     const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);//editorRef.current.getModel()-> monaco specific; allows to see changes happening in Monaco
+     initializedRoomsRef.current.add(roomId);
      //Bind YJS to Monaco
 
   }
+
+  
   return (
-   <div className='flex justify-center'>
+   <>
+   <Navbar/>
+   <div className='flex items-stretch'>
+    <div className='w-1/2 pl-4'>
+       <h1>Hello Coder!</h1>
+       Your Current Room Id is : {roomId}
+    </div>
     <Editor
       height="100vh"
-      width="100vw"
+      width="50vw"
       theme="vs-dark"
       onMount={handleEditorDidMount}
     />
   </div> 
+  </>
 )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,91 @@
-import React from 'react'
-import IDE from '../components/IDE'
-import Navbar from '../components/Navbar'
+import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import { v4 as uuid } from "uuid";
 
 function Landing() {
+  const [roomId,setRoomId] = useState("");
+  const navigate = useNavigate();
+  const handleCreateRoom = () => {
+    const newRoomId = Math.random().toString(36).substring(2, 7);
+    setRoomId(newRoomId);
+    navigate(`/room/${newRoomId}`,{state:{roomId: newRoomId}});
+  };
+
+  const handleJoinRoom = () => {
+    if(roomId) {
+      navigate(`/room/${roomId}`,{state:{roomId}});
+    }else{
+      navigate('/');
+    }
+  };
+
   return (
     <>
     <Navbar/>
-    <IDE/>
+    <div className="grid justify-items-center">
+      <h2 className="mt-4">Welcome</h2>
+      <div className="block p-6 rounded-lg shadow-lg bg-white max-w-lg mt-6">
+  <form>
+    <div className="form-group mb-6">
+      <label htmlFor="exampleInputRoom1" className="form-label inline-block mb-2 text-gray-700">Enter Room ID</label>
+      <input type="text" className="form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputRoom1"
+        aria-describedby="RoomIdHelp" onChange={(e) => setRoomId(e.target.value)}
+        placeholder="Enter room ID"/>
+    </div>
+    <div className='flex justify-between items-center'>
+    <button type="submit" className="
+      px-6
+      py-2.5
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-blue-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out" onClick={handleCreateRoom}>Create Room</button>
+      <button type="submit" className="
+      px-6
+      py-2.5
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700 hover:shadow-lg
+      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+      active:bg-blue-800 active:shadow-lg
+      transition
+      duration-150
+      ease-in-out" onClick={handleJoinRoom}>Join Room</button>
+    </div>  
+  </form>
+</div>
+    </div>
     </>
   )
 }
