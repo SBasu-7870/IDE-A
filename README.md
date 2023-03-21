5b3aea4eca2db27888593f80ec52ac96b77936b7
SBasu-7870 <rick05basu@gmail.com>
2023-03-21 18:42:31 +0530

    added readme + some stuff

---
+++ README.md
@@ -0,0 +1,41 @@
+IDE-A
+=================
+
+IDE-A is a collaborative text editor built on ReactJS frontend with Yjs, WebRTC, PeerJS, and Tailwind CSS. This project enables real-time document editing between users in a room and also provides a voice call feature between them.
+
+=================
+
+## Getting Started
+
+#### To use IDE-A, follow these steps:
+
+    Fork the repository to your GitHub account.
+    Clone the forked repository to your local machine.
+    Install the required dependencies by running ```npm i``` in the project directory.
+    Install PeerJS globally by running ```npm i peer -g.```
+    Start the PeerJS server by running the following command: ```peerjs --port 9000 --key peerjs --path /.```
+    Start the client by running ```npm run dev.```
+
+## Dependencies
+
+IDE-A relies on the following dependencies:
+
+    ReactJS
+    Yjs
+    WebRTC
+    PeerJS
+    Tailwind CSS
+
+## Usage
+
+Once you have followed the steps in the "Getting Started" section, you can use IDE-A to collaborate on documents with others in real-time. To start a new document collaboration session, open the app and create a new room. You can then share the room link with others who you want to collaborate with. Once they join the room, everyone can edit the document in real-time.
+
+IDE-A also provides a voice call feature that allows you to communicate with others in the same room. To start a voice call, simply click the call button in the app.
+
+## Contributing
+
+If you want to contribute to IDE-A, please feel free to submit a pull request. We welcome all contributions, including bug fixes, feature requests, and code improvements.
+
+## License
+
+IDE-A is licensed under the MIT License. You can find more information in the LICENSE file in the project directory.
--- src/components/IDE.jsx
+++ src/components/IDE.jsx
@@ -13,8 +13,11 @@ import { IndexeddbPersistence } from 'y-indexeddb';
 
 
 function IDE() {
-  const [localStream, setlocalStream] = useState({});
+  const [localStream, setlocalStream] = useState({}); //store localstream
+  const [currentPeer, setCurrentPeer] = useState(null);//store the current peer instance
+  const [currentPeerId, setCurrentPeerId] = useState(null);//store the current peer id. This is obtained from Yjs and is used to determine the current peer
   const [peers,setPeers] = useState([]); // keep track of connected peers
+  const [users, setUsers] = useState(null); //keep track of users in the room (Yjs)
   const location = useLocation();
   const roomId = location.state.roomId;
   
@@ -23,77 +26,142 @@ function IDE() {
   //Editor->value = YJS Text Value(A text value shared by multiple people)
   // One person deletes text -> Deletes from overall shared text value
   // Handled by YJS (a high performance CRDT(Conflict-Free Replicated Data Type))
-  
-  useEffect(()=>{
-    const createPeer = (peerId) => {
-      const peer = new Peer(peerId); // Create new Peer instance
-      peer.on('open', () => {
-        console.log('Peer connected with ID:', peer.id);
-        setPeers([...peers, peer]); // Add new peer instance to state
-      });
-    };
-      peers.forEach((peerId) => {
-        createPeer(peerId);
-      });
-    return () =>{
-       peers.forEach((peer)=>{
-        peer.destroy();
-       })
-    }
 
-  },[localStream,peers])
+  const usercolors = [
+    '#30bced',
+    '#6eeb83',
+    '#ffbc42',
+    '#ecd444',
+    '#ee6352',
+    '#9ac2c9',
+    '#8acb88',
+    '#1be7ff'
+  ]
 
+  const myColor = usercolors[Math.floor(Math.random() * usercolors.length)];
 
   const handleEditorDidMount = (editor,monaco) => { //editor instance and monaco from the <Editor/> (defined by Monaco Editor) 
     editorRef.current = editor;  //Initialise Yjs
     
 
      const doc = new Y.Doc(); //collection of shared objects -> Text
+     const yarray = doc.getArray(roomId);
      
-     //get the audio stream from the user in the room
-     navigator.mediaDevices.getUserMedia({audio:true})
-     .then((stream)=>{
-      setlocalStream(stream);
-     })
-     .catch((error)=>{
-      console.error("Failed to get user media:",error);
-     })
-
      const indexeddbProvider = new IndexeddbPersistence(roomId, doc);
      indexeddbProvider.on('synced', () => {
       console.log('content from the database is loaded')
     })
    
+    navigator.mediaDevices.getUserMedia({audio: true})
+    .then((stream)=>{
+      setlocalStream(stream);
+    })
+    .catch((err)=>{
+      console.log("Error in fetching media stream:",err);
+    })
     //  console.log(localStream);
      //Connect to peers (or start connection) with WebRTC
-     const provider = new WebrtcProvider(roomId,doc,{
-      peerOpts: {
-         stream: localStream
-      }
-    }); //room1, room2
-     console.log(provider.awareness);
+     const provider = new WebrtcProvider(roomId,doc); //room1, room2
+    console.log(provider.awareness);
+    setCurrentPeerId(provider.awareness.clientID);
+    provider.awareness.setLocalStateField('user',{name: "user0", color: myColor, id: provider.awareness.clientID});
+    
+    // setAwarenessState(provider.awareness);
+
+    provider.awareness.on('update',()=>{
+      const strings = [];
+    
+      provider.awareness.getStates().forEach(state=>{
+        if(state.user){
+          strings.push({color: state.user.color,name: state.user.name, id: state.user.id});
+        }
+        
+      })
+      setUsers(strings);
+    })
      const type = doc.getText("monaco"); // doc {"monaco" : "what our IDE is showing"}
       
      //Bind YJS to Monaco 
      const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness);//editorRef.current.getModel()-> monaco specific; allows to see changes happening in Monaco
      
-    
-
-     // add stream id to awareness state
-     provider.awareness.setLocalStateField('mediaStreamId', localStream.id);
   } 
-  
-  const handleCallButtonClick = () => {
-    // initiate call to all connected peers
-    Object.keys(peers).forEach(peerId => {
-      const peerConnection = peers[peerId];
-      const call = peerConnection.call(peerId, localStream);
-      call.on('stream', remoteStream => {
-        // handle incoming audio stream
-        console.log(`Received audio stream from peer ${call.peer}`);
-      });
+
+  useEffect(()=>{
+    console.log(localStream);
+    const peer = new Peer(currentPeerId,{
+      key: "peerjs",
+      host: "localhost",
+		  port: 9000,
+		  path: "/",
     });
+    peer.on("open", () => {
+      setCurrentPeer(peer);
+      setPeers((prevPeers)=> [...prevPeers,peer]);
+      console.log("MY peerJS id is:", peer.id);
+    });
+
+    peer.on("call",incoming=>{
+      console.log(incoming);
+      incoming.answer(localStream);
+      incoming.on('stream',stream=>{
+        // console.log(stream);
+        const audio = new Audio();
+        audio.srcObject = stream;
+        audio.play();
+      })
+    });
+    
+    peer.on('close',()=>{
+      peer.destroy();
+    })
+  },[currentPeerId,localStream]);
+
+  
+
+  const handleCall = () => {
+    if(!users || !currentPeer){
+      alert("Please wait");
+      return;
+    }
+
+      users.forEach((user)=>{
+        if(user.id != currentPeerId){
+          const peerConnection = new Peer(user.id,{
+            key: "peerjs",
+            host: "localhost",
+		        port: 9000,
+		        path: "/",
+          });
+          peerConnection.on('open',()=>{
+            setPeers((prevPeers)=> [...prevPeers,peerConnection]);
+            console.log("connected to peer id: "+ peerConnection.id);
+          })
+          const outgoing = currentPeer.call(peerConnection.id,localStream); //call all the peers present in the room
+          console.log(outgoing);
+          outgoing.on("open",()=>{
+            console.log("Outgoing call");
+          })
+
+          
+          outgoing.on('stream',(stream)=>{
+            console.log(stream);
+            const audio = new Audio();
+            audio.srcObject = stream;
+            audio.play();
+          })
+
+          outgoing.on('error',(err)=>{
+          console.error(`error in establishing call to ${peerConnection.id} :`,err);
+          })
+        }
+      })
+    
   }
+
+
+
+
+
   return (
    <>
    <Navbar/>
@@ -103,12 +171,14 @@ function IDE() {
        Your Current Room Id is : {roomId}
        <br/>
        <h2 >The users in this room are:</h2>
-      <ul>
-        {Object.keys(peers).map(peerId => (
-          <li key={peerId}>{peerId}</li>
-        ))}
-      </ul>
-      <Button variant="filled">Call Peers</Button>
+      {users && <ul>
+      {users.map(user => (
+      <li key={user.id} style={{ color: user.color }}>
+        {user.name}
+      </li>
+      ))}
+      </ul>}
+      <Button variant="filled" onClick={handleCall}>Call Peers</Button>
     </div>
     <Editor
       height="100vh"
